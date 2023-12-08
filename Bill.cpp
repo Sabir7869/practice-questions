60b0172ad4d1922cf71897783d0886004acd7f05
Sabir7869 <sa15076332@gmail.com>
2023-12-09 00:04:58 +0530

    practice question

--- Bill.cpp
+++ Bill.cpp
@@ -2,11 +2,11 @@
 using namespace std;
  int main()
 {
-    int pencil,pen,eraser;
+    float pencil,pen,eraser;
     cout<<"Enter the value of pencil,pen,eraser: ";
     cin>>pencil>>pen>>eraser;
-    int total=pen+pencil+eraser;
-    int gst=total*0.18;
+    float total=pen+pencil+eraser;
+    float gst=total*0.18;
     cout<<"Final bill is: "<<total+gst;
 return 0;
 
